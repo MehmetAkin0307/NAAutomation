a6b358ea6bff706c7f5c76969148caf931e807d9
Mehmet Akin <mehmetakin0307@gmail.com>
2019-09-17 20:34:39 -0500

    8.th commit

--- src/GitTest.java
+++ src/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
         System.out.println("3");
         System.out.println("6");
         System.out.println("7");
-        System.out.println("8");            
+        System.out.println("8");
+        System.out.println("9");
     }
 }
