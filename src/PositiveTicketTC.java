def8df0458ec0031ef8b14d900cfb3fc923986f1
Mehmet Akin <mehmetakin0307@gmail.com>
2019-09-22 15:29:37 -0500

    16.th commit

---
+++ src/PositiveTicketTC.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class PositiveTicketTC {
+    public static void main(String[] args) throws InterruptedException {
+        Browser myBrowser=new Browser();
+        WebDriver driver=myBrowser.invokeChromeBrowser();
+        driver.get("http://www.facebook.com");
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//input[@type='email']")).sendKeys("mehmetakin0307@gmail.com");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("pwd");
+
+    }
+}
